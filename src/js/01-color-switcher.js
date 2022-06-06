882bd4d495c4fc68586c53d83cc18a907c3a690a
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-06-06 23:48:04 +0300

    Виконав завдання-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const refs = {
+    body: document.body,
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+const DELAY = 1000;
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartChangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+
+    setDisabled(refs.startBtn);
+    removeDisabled(refs.stopBtn);
+
+    timerId = setInterval(changeColor, DELAY);
+};
+
+function onStopChangeColor() {
+
+    setDisabled(refs.stopBtn);
+    removeDisabled(refs.startBtn);
+
+    clearInterval(timerId);
+};
+
+function changeColor() {
+    const color = getRandomHexColor();
+
+    refs.body.style.backgroundColor = color;
+};
+
+function setDisabled(e) {
+    e.setAttribute('disabled', 'disabled');
+};
+
+function removeDisabled(e) {
+    e.removeAttribute('disabled');
+};
 
